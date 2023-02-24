e6a1cb5a1718898a4a53ff265ace25b952dccd3a
Raghu K <raghuk.vit@gmail.com>
2023-02-24 08:41:25 +0530

    Update learn-terraform/

--- roboshop-setup/ex1/servers.tf
+++ roboshop-setup/ex1/servers.tf
@@ -1,5 +1,12 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+
 resource "aws_instance" "frontend" {
-  ami                    = "ami-0a017d8ceb274537d"
+  ami                    = data.aws_ami.ami.image_id
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
   tags = {
@@ -34,7 +41,7 @@ resource "aws_route53_record" "cart" {
 
 resource "aws_instance" "catalogue" {
   ami                    = "ami-0a017d8ceb274537d"
-  instance_type          = "t3.micro"
+  instance_type          = "t3.small"
   vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
   tags = {
     Name = "catalogue"
@@ -170,5 +177,5 @@ resource "aws_route53_record" "rabbitmq" {
 }
 
 output "rabbitmq" {
-  value = "Public IP address  = ${aws_instance.rabbitmq.public_ip}
+  value = "Public IP address  = ${aws_instance.rabbitmq.public_ip}"
 }
