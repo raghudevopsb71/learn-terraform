2075f500e91dbd56ec129fea6d0670e4d98b76f2
Raghu K <raghuk.vit@gmail.com>
2023-02-25 08:29:29 +0530

    Update learn-terraform/

--- 04-vars/var.tf
+++ 04-vars/var.tf
@@ -1,48 +1,64 @@
-variable "sample" {
-  default = 100
-}
+//variable "sample" {
+//  default = 100
+//}
+//
+//output "sample" {
+//  value = var.sample
+//}
+//
+//variable "sample1" {}
+//
+//output "sample1" {
+//  value = var.sample1
+//}
+//
+//variable "cli" {}
+//
+//output "cli" {
+//  value = var.cli
+//}
+//
+//variable "input" {}
+//
+//
+//variable "sample5" {
+//  default = "Hello"
+//}
+//
+//variable "sample6" {
+//  default = [
+//    "Hello",
+//    1000,
+//    true,
+//    "World"
+//  ]
+//}
+//
+//variable "sample7" {
+//  default = {
+//    string  = "Hello",
+//    number  = 100,
+//    boolean = true
+//  }
+//}
+//
+//output "types" {
+//  value = "Variable sample5 - ${var.sample5}, First value in list - ${var.sample6[0]}, Boolean Value of Map = ${var.sample7["boolean"]}"
+//}
 
-output "sample" {
-  value = var.sample
-}
-
-variable "sample1" {}
-
-output "sample1" {
-  value = var.sample1
-}
-
-variable "cli" {}
-
-output "cli" {
-  value = var.cli
-}
-
-variable "input" {}
-
-
-variable "sample5" {
-  default = "Hello"
-}
-
-variable "sample6" {
+variable "d1" {
   default = [
-    "Hello",
-    1000,
-    true,
-    "World"
+    {
+      course_name  = "aws"
+      trainer_name = "raju"
+    },
+    {
+      course_name = "devops"
+      course_name = "ravi"
+    }
   ]
 }
 
-variable "sample7" {
-  default = {
-    string  = "Hello",
-    number  = 100,
-    boolean = true
-  }
+output "course_names" {
+  value = var.d1.*.course_name
 }
-
-output "types" {
-  value = "Variable sample5 - ${var.sample5}, First value in list - ${var.sample6[0]}, Boolean Value of Map = ${var.sample7["boolean"]}"
-}
-
