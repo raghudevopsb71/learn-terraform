e34e8311fb03b47b72760f3a7f2ff76270432195
Raghu K <raghuk.vit@gmail.com>
2023-02-25 08:51:33 +0530

    Update learn-terraform/

--- roboshop-setup/ex3/main.tf
+++ roboshop-setup/ex3/main.tf
@@ -5,26 +5,68 @@ data "aws_ami" "ami" {
 }
 
 
-resource "aws_instance" "instances" {
-  for_each               = var.instances
-  ami                    = data.aws_ami.ami.image_id
-  instance_type          = each.value["type"]
-  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
-  tags = {
-    Name = each.value["name"]
-  }
-}
+//resource "aws_instance" "instances" {
+//  for_each               = var.instances
+//  ami                    = data.aws_ami.ami.image_id
+//  instance_type          = each.value["type"]
+//  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+//  tags = {
+//    Name = each.value["name"]
+//  }
+//}
+//
+//variable "instances" {
+//  default = {
+//    catalogue = {
+//      name = "catalogue"
+//      type = "t3.micro"
+//    }
+//    user = {
+//      name = "user"
+//      type = "t3.small"
+//    }
+//  }
+//}
 
-variable "instances" {
-  default = {
-    catalogue = {
+## Immature code
+//variable "names" {
+//  default = [ "catalogue", "user"]
+//}
+//
+//variable "types" {
+//  default = ["t3.micro", "t3.small"]
+//}
+//
+//resource "aws_instance" "instances" {
+//  count = length(var.names)
+//  ami                    = data.aws_ami.ami.image_id
+//  instance_type          = var.types[count.index]
+//  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+//  tags = {
+//    Name = var.names[count.index]
+//  }
+//}
+
+## Little mature code
+variable "demo" {
+  default = [
+    {
       name = "catalogue"
       type = "t3.micro"
-    }
-    user = {
+    },
+    {
       name = "user"
       type = "t3.small"
     }
-  }
+  ]
 }
 
+resource "aws_instance" "instances" {
+  count                  = length(var.demo)
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = var.demo[count.index]["type"]
+  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+  tags = {
+    Name = var.demo[count.index]["name"]
+  }
+}
