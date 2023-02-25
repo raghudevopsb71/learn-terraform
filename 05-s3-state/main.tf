f784165d6c5dcce50f47ca2ba1fe782c36e20b44
Raghu K <raghuk.vit@gmail.com>
2023-02-25 08:06:50 +0530

    Update learn-terraform/

--- 05-s3-state/main.tf
+++ 05-s3-state/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "ec2" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
   tags = {
-    Name = "test"
+    Name = "demo"
   }
 }
 
