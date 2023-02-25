77e3d5b259ac2e906f41221b6e9a6ad151c8b752
Raghu K <raghuk.vit@gmail.com>
2023-02-25 08:53:43 +0530

    Update learn-terraform/

--- roboshop-setup/ex3/main.tf
+++ roboshop-setup/ex3/main.tf
@@ -5,28 +5,28 @@ data "aws_ami" "ami" {
 }
 
 
-//resource "aws_instance" "instances" {
-//  for_each               = var.instances
-//  ami                    = data.aws_ami.ami.image_id
-//  instance_type          = each.value["type"]
-//  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
-//  tags = {
-//    Name = each.value["name"]
-//  }
-//}
-//
-//variable "instances" {
-//  default = {
-//    catalogue = {
-//      name = "catalogue"
-//      type = "t3.micro"
-//    }
-//    user = {
-//      name = "user"
-//      type = "t3.small"
-//    }
-//  }
-//}
+resource "aws_instance" "instances" {
+  for_each               = var.instances
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = each.value["type"]
+  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+  tags = {
+    Name = each.value["name"]
+  }
+}
+
+variable "instances" {
+  default = {
+    catalogue = {
+      name = "catalogue"
+      type = "t3.micro"
+    }
+    user = {
+      name = "user"
+      type = "t3.small"
+    }
+  }
+}
 
 ## Immature code
 //variable "names" {
@@ -48,25 +48,25 @@ data "aws_ami" "ami" {
 //}
 
 ## Little mature code
-variable "demo" {
-  default = [
-    {
-      name = "catalogue"
-      type = "t3.micro"
-    },
-    {
-      name = "user"
-      type = "t3.small"
-    }
-  ]
-}
-
-resource "aws_instance" "instances" {
-  count                  = length(var.demo)
-  ami                    = data.aws_ami.ami.image_id
-  instance_type          = var.demo[count.index]["type"]
-  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
-  tags = {
-    Name = var.demo[count.index]["name"]
-  }
-}
+//variable "demo" {
+//  default = [
+//    {
+//      name = "catalogue"
+//      type = "t3.micro"
+//    },
+//    {
+//      name = "user"
+//      type = "t3.small"
+//    }
+//  ]
+//}
+//
+//resource "aws_instance" "instances" {
+//  count                  = length(var.demo)
+//  ami                    = data.aws_ami.ami.image_id
+//  instance_type          = var.demo[count.index]["type"]
+//  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+//  tags = {
+//    Name = var.demo[count.index]["name"]
+//  }
+//}
