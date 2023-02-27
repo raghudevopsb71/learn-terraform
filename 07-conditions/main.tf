74b7b001fc3882026e9fa159aab3db83ae88933a
Raghu K <raghuk.vit@gmail.com>
2023-02-27 07:52:30 +0530

    Update learn-terraform/

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -4,3 +4,12 @@ resource "aws_instance" "test" {
 }
 
 variable "instance_type" {}
+
+variable "create_instance" {}
+
+resource "aws_instance" "test" {
+  count         = var.create_instance == "true" ? 1 : 0
+  ami           = "ami-0a017d8ceb274537d"
+  instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
+}
+
