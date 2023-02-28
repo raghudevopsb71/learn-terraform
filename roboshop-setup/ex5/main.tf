82763217788b283493d8f38b71c9dae105870426
Raghu K <raghuk.vit@gmail.com>
2023-02-28 08:00:48 +0530

    Update learn-terraform/

--- roboshop-setup/ex5/main.tf
+++ roboshop-setup/ex5/main.tf
@@ -11,8 +11,10 @@ module "ec2" {
 }
 
 module "route53" {
+  for_each   = var.instances
   source     = "./route53"
-  private_ip = module.ec2.private_ip
+  component  = each.value["name"]
+  private_ip = module.ec2
 }
 
 
