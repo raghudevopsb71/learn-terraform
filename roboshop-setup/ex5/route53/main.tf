4b38d8ad69b6552cfffd8552ffcebe4246e4a73e
Raghu K <raghuk.vit@gmail.com>
2023-02-28 08:03:00 +0530

    Update learn-terraform/

--- roboshop-setup/ex5/route53/main.tf
+++ roboshop-setup/ex5/route53/main.tf
@@ -1,10 +1,12 @@
-//resource "aws_route53_record" "record" {
-//  zone_id = "Z103214126L48SQW30RSR"
-//  name    = "${var.component}-dev.devopsb71.online"
-//  type    = "A"
-//  ttl     = 30
-//  records = [aws_instance.ec2.private_ip]
-//}
+resource "aws_route53_record" "record" {
+  zone_id = "Z103214126L48SQW30RSR"
+  name    = "${var.component}-dev.devopsb71.online"
+  type    = "A"
+  ttl     = 30
+  records = [var.private_ip]
+}
 
 variable "private_ip" {}
+variable "component" {}
+
 
