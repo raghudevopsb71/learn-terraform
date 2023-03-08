914f672bf9b005aca06d329ba7951b11ceea1320
Raghu K <raghuk.vit@gmail.com>
2023-03-08 08:21:07 +0530

    Update learn-terraform/

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -9,6 +9,11 @@ resource "aws_spot_instance_request" "node1" {
   }
 }
 
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-server"
+}
 
 resource "aws_spot_instance_request" "node2" {
   ami                    = "ami-0a36d3747768bc34b"
@@ -18,4 +23,10 @@ resource "aws_spot_instance_request" "node2" {
   tags = {
     Name = "prom-test-node"
   }
+}
+
+resource "aws_ec2_tag" "node2" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-node"
 }
